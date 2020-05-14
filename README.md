38cdcd1d9c75f215717e3a13b4a0dfc493c3c3fd
falkn <k.fraissine@gmail.com>
2020-05-14 18:43:36 +0200

    rd

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # ylauncher
 
-A simple python script, based on selenium, to automatically launch the last youtube video of a channel.
+YLauncher is a Python script to automatically launch the last youtube video of a channel.
+
+## Installation
+
+Install python3
+Install selenium
+Clone repo
+
+## Usage
+
+```python
+> python3 main.py -u <user_channel_name>
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
+import sys, getopt, time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -9,25 +10,52 @@ class YLauncher:
 	web_driver_wait_time = 1200
 	ad_sleep_time = 5.5
 
-	def __init__(self, driver):
+	def __init__(self, driver, username):
 		self.driver = driver
 		self.wait = WebDriverWait(self.driver, self.web_driver_wait_time)
-		self.channel_name = "edsheeran"
+		self.username = username
 		self.skip_ad_button_klass = ".ytp-ad-skip-button"
 		self.video_ended_selector = "#movie_player.ended_mode"
 
 	def process(self):
+		self.play()
+
+	def play(self):
 		self.driver.maximize_window()
-		self.driver.get("https://www.youtube.com/user/" + self.channel_name + "/videos")
-		first_result = self.wait.until(presence_of_element_located((By.ID, "video-title")))
-		first_result.click()
+		self.driver.get("https://www.youtube.com/user/" + self.username + "/videos")
+		self.click_on_lastest_video()
 		self.driver.find_element(By.CSS_SELECTOR, 'body').send_keys('F')
-		skip_ad = self.wait.until(presence_of_element_located((By.CSS_SELECTOR, self.skip_ad_button_klass)))
-		time.sleep(self.ad_sleep_time)
-		skip_ad.click()
+		self.skip_ad()
 		self.wait.until(presence_of_element_located((By.CSS_SELECTOR, self.video_ended_selector)))		
 
+	def click_on_lastest_video(self):
+		first_result = self.wait.until(presence_of_element_located((By.ID, "video-title")))
+		first_result.click()
 
-with webdriver.Firefox() as driver:
-	y_launcher = YLauncher(driver)
-	y_launcher.process()
+	def skip_ad(self):
+		skip_ad_element = self.wait.until(presence_of_element_located((By.CSS_SELECTOR, self.skip_ad_button_klass)))
+		time.sleep(self.ad_sleep_time)
+		skip_ad_element.click()
+
+def main(argv):
+	username = "PassiveAggressions"
+
+	try:
+		opts, args = getopt.getopt(argv,"hu:",["username="])
+	except getopt.GetoptError:
+		print('test.py -u <username>')
+		sys.exit(2)
+
+	for opt, arg in opts:
+		if opt == '-h':
+			print('test.py -u <username>')
+			sys.exit()
+		elif opt in ("-u", "--username"):
+			username = arg
+
+	with webdriver.Firefox() as driver:
+		y_launcher = YLauncher(driver, username)
+		y_launcher.process()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
